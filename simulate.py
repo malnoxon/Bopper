663cfc9f5194458f264fcf1514bd4df569db846d
Daniel Daphron Kaczmarek <xenodaphron@gmail.com>
2014-04-12 13:59:45 -0500

    one arm working

--- simulate.py
+++ simulate.py
@@ -10,15 +10,16 @@ def create_caveman(world, x_pos, caveman):
     mbody_density = caveman.wBody / 2 *(1 * cm.HEIGHT/2)
     box = mbody.CreatePolygonFixture(box=(1, cm.HEIGHT/2), density=mbody_density , friction=0.3)
 
-    a1, aq = caveman.appendages
+    a1, a2 = caveman.appendages
 
-    bicep_body = world.CreateDynamicBody(position=(x_pos+a1.lBicep, caveman.arm_height), angle=pi/2)
+    bicep_body = world.CreateDynamicBody(position=(x_pos+a1.lBicep/2, caveman.arm_height), angle=pi/2)
     bicep_density = a1.wBicep / 2*(0.5 * a1.lBicep)
-    bicep = bicep_body.CreatePolygonFixture(box=(a1.wBicep, aq.lBicep/2), density=bicep_density, friction=0.3)
-    joint1 = world.CreateRevoluteJoint(bodyA=mbody, bodyB=bicep_body, localAnchorA=(0, cm.HEIGHT - caveman.arm_height/2), localAnchorB=(0, a1.lBicep/2), lowerAngle = -pi, upperAngle = pi, enableMotor=True)
+    bicep = bicep_body.CreatePolygonFixture(box=(0.5, a1.lBicep/2), density=bicep_density, friction=0.3)
+    joint1 = world.CreateRevoluteJoint(bodyA=mbody, bodyB=bicep_body, localAnchorA=(0,caveman.arm_height - cm.HEIGHT/2.0) , localAnchorB=(0, a1.lBicep/2), lowerAngle = -pi, upperAngle = pi, enableMotor=True)
+    # joint1 = world.CreateRevoluteJoint(bodyA=mbody, bodyB=bicep_body, localAnchorA=(0, cm.HEIGHT/2 - caveman.arm_height) , localAnchorB=(0, a1.lBicep/2), lowerAngle = -pi, upperAngle = pi, enableMotor=True)
 
     forearm_width = 0.5
-    forearm_body = world.CreateDynamicBody(position=(x_pos+a1.lForearm, caveman.arm_height), angle=pi/2)
+    forearm_body = world.CreateDynamicBody(position=(x_pos+a1.lBicep, caveman.arm_height), angle=pi/2)
     forearm_density = a1.wForearm / 2*(0.5 * a1.lForearm)
     forearm = forearm_body.CreatePolygonFixture(box=(0.5, a1.lForearm/2), density=forearm_density, friction=0.3)
 
@@ -28,6 +29,7 @@ def create_caveman(world, x_pos, caveman):
     bopper_body = world.CreateDynamicBody(position=(x_pos+a1.lBicep+a1.lForearm, caveman.arm_height), angle=0)
     bopper_density = a1.wBopper / pi*(a1.rBopper)**2
     bopper = bopper_body.CreateCircleFixture(radius=bopper_radius, density=bopper_density, friction=0.3)
+    joint3=world.CreateRevoluteJoint(bodyA=forearm_body, bodyB=bopper_body, localAnchorA=(0, -a1.lForearm/2), localAnchorB=(0,0), lowerAngle=-pi, upperAngle=pi)
 
     return [mbody, bicep_body, forearm_body, bopper_body]
 
@@ -49,12 +51,12 @@ def simulate(caveman1, caveman2, graphics_enabled):
 
     bodies1 = create_caveman(world, 5, caveman1)
     bodies2 = create_caveman(world, 25, caveman2)
+    all_bodies = bodies1 + bodies2 + [groundBody]
 
     boppers = [bodies1[3], bodies2[3]]
     bodies1 = bodies1[:-1]
     bodies2 = bodies2[:-1]
     
-    all_bodies = bodies1 + bodies2 + [groundBody]
 
 
     timeStep = 1.0 / FPS
